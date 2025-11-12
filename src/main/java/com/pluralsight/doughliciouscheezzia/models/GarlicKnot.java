b99343193a09cacf20f0c6c2b08c6e46492873ef
Qi Qing <learn.qiqing@gmail.com>
2025-11-12 14:28:59 -0500

    completed GarlicKnot.java

--- src/main/java/com/pluralsight/doughliciouscheezzia/models/GarlicKnot.java
+++ src/main/java/com/pluralsight/doughliciouscheezzia/models/GarlicKnot.java
@@ -1,4 +1,14 @@
 package com.pluralsight.doughliciouscheezzia.models;
 
 public class GarlicKnot extends MenuItem {
+
+    public GarlicKnot(String name) {
+        super("Garlic Knots - (set of 3)");
+    }
+
+    @Override
+    public double calculatePrice() {
+        return 1.50;
+    }
+
 }
