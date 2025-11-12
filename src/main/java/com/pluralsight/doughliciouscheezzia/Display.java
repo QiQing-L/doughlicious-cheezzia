57f92dab7e227fa137682e147fd15b5988aff8b2
Qi Qing <learn.qiqing@gmail.com>
2025-11-12 16:03:13 -0500

    worked on Display.java: added attribute, added list of planned methods as reminder in comment, and some method stubs.

--- src/main/java/com/pluralsight/doughliciouscheezzia/Display.java
+++ src/main/java/com/pluralsight/doughliciouscheezzia/Display.java
@@ -1,7 +1,41 @@
 package com.pluralsight.doughliciouscheezzia;
 
+import com.pluralsight.doughliciouscheezzia.pos.Order;
+
+import java.util.Scanner;
+
 public class Display {
+    private Order currentOrder;
+
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+     /*
+        + main()
+        + loadMenu() : viod
+        + displayMenu() : void
+        + homeScreen(scanner : Scanner) : void
+        + orderScreen(scanner : Scanner) : void
+        + createOrder(scanner : Scanner) : Order
+        + checkOut(order : Order)
+        + addPizza(scanner : Scanner) : void
+        + addDrinks(scanner : Scanner) : void
+        + addGarlicKnots(scanner : Scanner) : void
+         */
+    public void loadMenu(){
+
+    }
+    public void displayMenu(){
+
+    }
+
+    public void homeScreen(Scanner scanner){
+
+    }
+
+    public void orderScreen(Scanner scanner){
 
     }
 }
