5e2fd89a44f11b8185639b61b63453ea00681e38
Qi Qing <learn.qiqing@gmail.com>
2025-11-12 14:21:22 -0500

    worked on Drink.java. add attribute, constructor and override calculatePrice

--- src/main/java/com/pluralsight/doughliciouscheezzia/models/Drink.java
+++ src/main/java/com/pluralsight/doughliciouscheezzia/models/Drink.java
@@ -1,10 +1,28 @@
 package com.pluralsight.doughliciouscheezzia.models;
 
 public class Drink extends MenuItem {
+    private String size;
 
+    public Drink(String name, String size) {
+        super(name);
+        this.size = size;
+    }
 
     @Override
     public double calculatePrice() {
-        return 0;
+        double drinkPrice = 0;
+        switch (size){
+            case "small":
+                drinkPrice = 2.00;
+                break;
+            case "medium":
+                drinkPrice = 2.50;
+                break;
+            case "large":
+                drinkPrice = 3.00;
+                break;
+        }
+
+        return drinkPrice;
     }
 }
